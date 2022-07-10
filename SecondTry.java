ca67c10d052a16080073e653047d26f09687d9f2
Iryna Ch <iryna.chesanovska@gmail.com>
2022-07-10 22:20:05 +0300

    Changes according to comments

--- SecondTry.java
+++ SecondTry.java
@@ -1,4 +1,4 @@
 public class SecondTry {
     public static void main(String[] args) {
-        System.out.println("Weekend learning.");}
+        System.out.println("Weekend learning. Rewritten");}
 }
