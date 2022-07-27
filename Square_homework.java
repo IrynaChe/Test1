2c19a7e18b7933d6c144fbdb4e5d30a57151168f
Iryna Ch <iryna.chesanovska@gmail.com>
2022-07-27 21:24:11 +0300

    1st part

---
+++ Square_homework.java
@@ -0,0 +1,10 @@
+public class Square_homework {
+    public static void main(String args[])
+    {
+        int radius = 5;
+        double area = Math.PI * (radius * radius);
+        System.out.println("Площа круга: " + area);
+        double circumference= Math.PI * 2*radius;
+        System.out.println("Довжина кола: " + circumference) ;
+    }
+}
